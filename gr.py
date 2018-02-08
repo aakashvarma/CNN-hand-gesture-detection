dc813911377814127fb79a8bba43c3d83a059c69
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-09 03:15:06 +0530

    Finished the training sode , but not trained (Might have errors in the code)

--- gr.py
+++ gr.py
@@ -1,11 +1,14 @@
 #%%
 
+import os
 import cv2
 import urllib
 import numpy as np
+from PIL import Image
 import tensorflow as tf
 from urllib.request import urlopen
 from matplotlib import pyplot as plt
+from sklearn.cross_validation import train_test_split
 
 #%%
 def vid_input:
@@ -42,10 +45,11 @@ nb_epoch = 15
 nb_filters = 32
 pool_shape = 2
 filter_shape = 3
+learning_rate = 0.0001
 output = ["OK", "NOTHING","PEACE", "PUNCH", "STOP"]
 
 #%%
-
+#surf sift orb
 x = tf.placeholder(tf.float32, [None, img_rows*img_cols])
 x_shaped = tf.reshape(x, [1, img_rows, img_cols, 1])
 y = tf.placeholder(tf.float32, [None, 10])
@@ -87,9 +91,100 @@ y_ = tf.nn.softmax(dense_layer2)
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = dense_layer2, labels = y))
 
+#%%
+
+def modlistdir(path):
+    listing = os.listdir(path)
+    retlist = []
+    for name in listing:
+        if name.startswith('.'):
+            continue
+        retlist.append(name)
+    return retlist
+
+
+    path2 = './leapmotion_dataset'
+    imlist = modlistdir(path2)
+        
+    image1 = np.array(Image.open(path2 +'/' + imlist[0])) 
+
+    m,n = image1.shape[0:2] 
+    total_images = len(imlist) 
+
+    immatrix = np.array([np.array(Image.open(path2+ '/' + images).convert('L')).flatten()
+                            for images in imlist], dtype = 'f')
 
 
 
+    print (immatrix.shape)
+
+    raw_input("Press any key")
+        
+    label=np.ones((total_images,),dtype = int)
+
+    samples_per_class = total_images / nb_classes
+    print ("samples_per_class - ",samples_per_class)
+    s = 0
+    r = samples_per_class
+    for classIndex in range(nb_classes):
+        label[s:r] = classIndex
+        s = r
+        r = s + samples_per_class
+
+
+    data,Label = shuffle(immatrix,label, random_state=2)
+    train_data = [data,Label]
+        
+    (X, Y) = (train_data[0],train_data[1])
+        
+        
+    # Spltting
+        
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=4)
+        
+    X_train = X_train.reshape(X_train.shape[0], img_channels, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], img_channels, img_rows, img_cols)
+        
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+        
+    # normalize
+    X_train /= 255
+    X_test /= 255
+        
+    # convert class vectors to binary class matrices
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    return X_train, X_test, Y_train, Y_test
+
+
+#%%
+
+optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+init_op = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init_op)
+    total_batch = int(total_images / batch_size)
+    for epoch in range(epochs):
+        avg_cost = 0
+        for i in range(total_batch):
+            batch_x = tf.train.batch(X_train, batch_size=batch_size)
+            batch_y = tf.train.batch(Y_train, batch_size=batch_size)
+            _, c = sess.run([optimiser, cross_entropy], 
+                            feed_dict={x: batch_x, y: batch_y})
+            avg_cost += c / total_batch
+        test_acc = sess.run(accuracy, 
+                       feed_dict={x: X_test, y: y_test})
+        print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost), " 
+                 test accuracy: {:.3f}".format(test_acc))
+
+    print("\nTraining complete!")
+    print(sess.run(accuracy, feed_dict={x: X_test, y: Y_test}))
 
 
 
